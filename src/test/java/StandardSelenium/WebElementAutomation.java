bbe5fe25441f3831ee18c94ba5c04e1dd0a1e970
it21710810 <it21710810@my.sliit.lk>
2025-03-18 23:42:10 +0530

    Tested ComboBox

--- src/test/java/StandardSelenium/WebElementAutomation.java
+++ src/test/java/StandardSelenium/WebElementAutomation.java
@@ -1,6 +1,8 @@
 package StandardSelenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebElementAutomation {
@@ -15,8 +17,8 @@ public class WebElementAutomation {
 
 //       Calling methods
         OptionButton();
-//        CheckBoxes();
-//        ComboBoxes();
+        CheckBoxes();
+        ComboBoxes();
 //        DropDownList();
 //        Button();
 //        TextBox();
@@ -26,6 +28,39 @@ public class WebElementAutomation {
 
     }
 
+
     public static void OptionButton() {
+//        Identify the web element
+        WebElement option1 = driver.findElement(By.id("vfb-7-1"));
+        WebElement option2 = driver.findElement(By.id("vfb-7-2"));
+        WebElement option3 = driver.findElement(By.id("vfb-7-3"));
+
+//        Trigger the action
+        option1.click();
+
+//        Validation
+        if (option1.isSelected()) {
+            System.out.println("Option button 1 has been selected");
+        } else {
+            System.out.println("Option button 1 didn't selected");
+        }
+    }
+
+    public static void CheckBoxes() {
+        WebElement checkbox1 = driver.findElement(By.id("vfb-6-0"));
+        WebElement checkbox2 = driver.findElement(By.id("vfb-6-1"));
+        WebElement checkbox3 = driver.findElement(By.id("vfb-6-2"));
+
+        checkbox1.click();
+        checkbox2.click();
+
+        if (checkbox1.isSelected() && checkbox2.isSelected()) {
+            System.out.println("Check box 1 and 2 has been selected");
+        } else {
+            System.out.println("Check box 1 and 2 didn't selected");
+        }
+    }
+
+    public static void ComboBoxes() {
     }
 }
