347f58e5968edc36c3d72e9663dffd2a235a20a9
it21710810 <it21710810@my.sliit.lk>
2025-03-19 11:06:46 +0530

    Auto Test Facebook

--- src/test/java/StandardSelenium/FacebookLogin.java
+++ src/test/java/StandardSelenium/FacebookLogin.java
@@ -1,6 +1,8 @@
 package StandardSelenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FacebookLogin {
@@ -10,5 +12,21 @@ public class FacebookLogin {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.facebook.com/login.php/");
+
+        Thread.sleep(2000);
+
+        WebElement email_phoneTxt = driver.findElement(By.id("email"));
+        WebElement password_Txt = driver.findElement(By.id("pass"));
+        WebElement loginBtn = driver.findElement(By.name("login"));
+
+        email_phoneTxt.sendKeys("your email");
+        password_Txt.sendKeys("your password");
+        loginBtn.click();
+
+//        OR easy method
+//        driver.findElement(By.id("email")).sendKeys("your email);
+//        driver.findElement(By.id("pass")).sendKeys("your password");
+//        driver.findElement(By.name("login")).click();
+
     }
 }
